3d92230b31388706f171d644664f2154cae19a19
Volodymyr Ajentik <vie@ajentik.ai>
2026-03-14 19:58:17 +0800

    exp3: GELU activation instead of ReLU-squared

--- train.py
+++ train.py
@@ -141,7 +141,7 @@ class MLP(nn.Module):
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = F.relu(x).square()
+        x = F.gelu(x)
         x = self.c_proj(x)
         return x
 
